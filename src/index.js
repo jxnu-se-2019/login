8bce7e43658d52bf52d980b5cdb03581cef26f6f
jiangtao <457296124@qq.com>
2019-11-26 15:28:29 +0800

    modify

--- src/index.js
+++ src/index.js
@@ -2,19 +2,18 @@ import _ from 'lodash';
 import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.min.js'
-import './css/signin.css';
-import './lib/jquery.validate.min.js'
+import 'bootstrap/dist/js/bootstrap.min.js';
+import './style.less';
+import 'jquery-validation';
 
 
 $('form').validate({
-	rules:{
-		username:{
-			email:true
+	rules: {
+		username: {
+			email: true
 		},
-		password:{
-			minlength:6
+		password: {
+			minlength: 6
 		}
 	}
 });
-
--- webpack.config.js
+++ webpack.config.js
@@ -14,21 +14,34 @@ module.exports = {
 		}),
 		new webpack.HotModuleReplacementPlugin()
 	],
+	devServer: {
+		contentBase: path.join(__dirname, 'dist'),
+		compress: true,
+		port: 9000
+	},
 	module: {
 		rules: [{
-			test: /\.css$/,
-			use: [{
-					loader: 'style-loader'
-				},
-				{
-					loader: 'css-loader'
-				}
-			]
-		}, {
-			test: /\.(png|svg|jpg|gif)$/,
-			use: [
-				'file-loader'
-			]
-		}],
+				test: /\.css$/,
+				use: [
+					'style-loader',
+					'css-loader'
+				]
+			},
+			{
+				test: /\.(png|svg|jpg|gif)$/,
+				use: [
+					'file-loader',
+				]
+			},
+			{
+				test: /\.less$/,
+				use: [
+					'style-loader',
+					'css-loader',
+					'less-loader'
+				]
+			}
+
+		],
 	},
 };
