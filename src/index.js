a12256972b1a61acbe735e8b38824936febcd91c
jiangtao <457296124@qq.com>
2019-12-06 14:03:55 +0800

    homework

--- src/index.js
+++ src/index.js
@@ -1,21 +1,41 @@
 import _ from 'lodash';
 import $ from 'jquery';
+import ko from 'knockout';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
 import './style.less';
-import 'jquery-validation';
+import 'knockout';
+import 'knockout.validation';
 
-
-$('form').validate({
-	errorClass: "text-danger",
-	rules: {
-		username: {
-			email: true,
+var viewModel = {
+	username: ko.observable().extend({
+		required: {
+			message: '请输入username'
 		},
-		password: {
-			minlength: 6
-		},
+		email:{
+			message:'请输入正确的邮箱格式'
+		}
+	}),
+	password: ko.observable().extend({
+		required:{
+			message: '请输入password'
+		}
+	}),
 
+	SubmitClick: function() {
+		
+		alert($('#username').val());
+		alert($('#password').val());
+		if (viewModel.errors().length == 0) {
+			return;
+		} else {
+			viewModel.errors.showAllMessages();
+			return false;
+		}
 	}
-});
+};
+
+viewModel.errors = ko.validation.group(viewModel);
+
+ko.applyBindings(viewModel);
